02c89751c2762ffc7292f00782ec19f9dfeb4b4d
Pavel Volkov <pavel.volkov@arealidea.ru>
2025-03-16 21:17:47 +0300

    feat: add routing

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-} 
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message": "Hello, World!"}`))
+	})
+
+	fmt.Println("Сервер запущен на http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
